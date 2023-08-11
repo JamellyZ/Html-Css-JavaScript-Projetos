18768f7e0bb22dc571b13be99d216b915f9be880
jamellyZ <jamelly3@hotmail.com>
2023-08-11 13:05:35 -0300

    ajust in random

--- script-jogo.js
+++ script-jogo.js
@@ -1,7 +1,7 @@
 function aleatorio(){
     var dados = ["","pedra", "papel", "tesoura"]
     let randomInt = dados[Math.ceil(Math.random() * (3))]    
-    return random
+    return randomInt
 }
 
 function limpar(){
@@ -17,24 +17,24 @@ function opcoes(empate,derrota){
     var escolha = document.getElementById(empate)
     var resultado = document.getElementById("resultado")
     var resposta = document.getElementById("foto")
-    random = aleatorio()
-    resultado.innerHTML = random
+    randomInt = aleatorio()
+    resultado.innerHTML = randomInt
     limpar()
-    if(random == empate){
+    if(randomInt == empate){
         resultado.innerHTML = resultado.setAttribute("src","empate.png")
         document.body.style.background = "#EEDD82"
         escolha.innerHTML = escolha.setAttribute("src", empate + "-empate.png")
         resposta.innerHTML = resposta.setAttribute("src", empate + "-empate.png")
-    }else if(random == derrota){
+    }else if(randomInt == derrota){
         resultado.innerHTML = resultado.setAttribute("src", "derrota.png" )
         document.body.style.background = "#CD4F39"
         escolha.innerHTML = escolha.setAttribute("src", empate + "-derrota.png")
-        resposta.innerHTML = resposta.setAttribute("src", random + "-derrota.png")
+        resposta.innerHTML = resposta.setAttribute("src", randomInt + "-derrota.png")
     }else{
         resultado.innerHTML = resultado.setAttribute("src", "vitoria.png" )
         document.body.style.background = "#00FF7F"
         escolha.innerHTML = escolha.setAttribute("src", empate + "-vitoria.png")
-        resposta.innerHTML = resposta.setAttribute("src", random + "-vitoria.png")
+        resposta.innerHTML = resposta.setAttribute("src", randomInt + "-vitoria.png")
     }
 }
 
